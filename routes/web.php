fcf4381c459b38db49a34a30e61ae09ed7056069
Eliab <35658748+eliabsiji@users.noreply.github.com>
2026-09-12 16:11:06 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -102,7 +102,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Auth::routes();
-Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 
 
