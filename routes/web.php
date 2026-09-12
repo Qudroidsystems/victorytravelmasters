ecc8f2e1f3287f88674db58d7781fba3a8fa82d4
Eliab <35658748+eliabsiji@users.noreply.github.com>
2026-09-12 15:57:16 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -96,9 +96,9 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 */
 
-Route::get('/', function () {
-    return redirect('/login');
-});
+// Route::get('/', function () {
+//     return redirect('/login');
+// });
 
 
 Auth::routes();
