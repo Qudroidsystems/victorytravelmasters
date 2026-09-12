221ebff34f82565a4ec63cab3e52433675599f2b
Eliab <35658748+eliabsiji@users.noreply.github.com>
2026-09-12 15:40:27 +0100

    working

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,22 +3,13 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '745336c4013961bdb7b1f95ca36f1a66ca01c4da',
+        'reference' => 'af94ad63f3a757d8e14731a6db8dada837451fad',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
         'dev' => true,
     ),
     'versions' => array(
-        'bacon/bacon-qr-code' => array(
-            'pretty_version' => '2.0.8',
-            'version' => '2.0.8.0',
-            'reference' => '8674e51bb65af933a5ffaf1c308a660387c35c22',
-            'type' => 'library',
-            'install_path' => __DIR__ . '/../bacon/bacon-qr-code',
-            'aliases' => array(),
-            'dev_requirement' => false,
-        ),
         'barryvdh/laravel-dompdf' => array(
             'pretty_version' => 'v3.1.1',
             'version' => '3.1.1.0',
@@ -78,15 +69,6 @@
             'replaced' => array(
                 0 => '*',
             ),
-        ),
-        'dasprid/enum' => array(
-            'pretty_version' => '1.0.7',
-            'version' => '1.0.7.0',
-            'reference' => 'b5874fa9ed0043116c72162ec7f4fb50e02e7cce',
-            'type' => 'library',
-            'install_path' => __DIR__ . '/../dasprid/enum',
-            'aliases' => array(),
-            'dev_requirement' => false,
         ),
         'davedevelopment/hamcrest-php' => array(
             'dev_requirement' => true,
@@ -523,7 +505,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '745336c4013961bdb7b1f95ca36f1a66ca01c4da',
+            'reference' => 'af94ad63f3a757d8e14731a6db8dada837451fad',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -976,6 +958,15 @@
             'aliases' => array(),
             'dev_requirement' => true,
         ),
+        'picqer/php-barcode-generator' => array(
+            'pretty_version' => 'v3.2.4',
+            'version' => '3.2.4.0',
+            'reference' => '16a5cfb74eb8b7a0aa2e60427d98599aa8259380',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../picqer/php-barcode-generator',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
         'pontedilana/php-weasyprint' => array(
             'pretty_version' => '1.5.0',
             'version' => '1.5.0.0',
@@ -1318,15 +1309,6 @@
             'install_path' => __DIR__ . '/../sebastian/version',
             'aliases' => array(),
             'dev_requirement' => true,
-        ),
-        'simplesoftwareio/simple-qrcode' => array(
-            'pretty_version' => '4.2.0',
-            'version' => '4.2.0.0',
-            'reference' => '916db7948ca6772d54bb617259c768c9cdc8d537',
-            'type' => 'library',
-            'install_path' => __DIR__ . '/../simplesoftwareio/simple-qrcode',
-            'aliases' => array(),
-            'dev_requirement' => false,
         ),
         'spatie/laravel-package-tools' => array(
             'pretty_version' => '1.92.7',
